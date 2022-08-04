f15460c8a61c08035f33c5128825af21846e68c9
MBraganzaa <josebraganza23@gmail.com>
2022-08-04 01:30:04 -0300

    add episodio 1 java dalto 3

--- aprendiendo javascript/historia de cofla/episodio3/ejercicio1.js
+++ aprendiendo javascript/historia de cofla/episodio3/ejercicio1.js
@@ -1,49 +1,50 @@
 class Celular {
-    costructor (color,peso,rdp,rdc,ram){
+    constructor (color,peso,rdp,rdc,ram){
         this.color = color;
         this.peso = peso;
         this.resolucionDePantalla = rdp;
         this.resolucionDeCamara = rdc;
         this.ram = ram;
         this.encender = false;
-        this.enc = false;
     }
     botonDeEncender (){
-        if (this.enc == false){
+        if (this.encender == false){
             alert("encendiendo el celular")
-            this.encender = true;
+            this.encender = true
         } else {
-            alert("celular apagado");
+            alert("apagando el celular")
             this.encender = false;
         }
     }
     reiniciar (){
         if (this.encender == true){
             alert("reiniciando el celular");
-            this.encender = false;
         } else {
             alert("el celular esta apagado");
         }
     }
     sacarFoto (){
         if (this.encender == true){
-            alert(`sacando una foto con una calidad : ${rdc}`);
+            alert(`sacando una foto con una calidad : ${this.resolucionDePantalla}`);
         } else {
             alert("el celular esta apagado");
         }
     }
     grabarVideo (){
         if (this.encender == true){
-            alert(`grabando un video con una calidad de : ${rdc}`);
+            alert(`grabando un video con una calidad de : ${this.resolucionDeCamara}`);
         } else {
             alert("el celular esta apagado");
         }
     }
 }
 
-const celular1 = new Celular ("rojo","150g","5.5","full hd","2GB");
+celular1 = new Celular ("rojo","150g","5.5'","hd","1GB");
+celular2 = new Celular ("gray","155g","5'","full hd","2GB");
+celular3 = new Celular ("black","140g","5.4'","full hd","4GB");
 
 celular1.botonDeEncender()
 celular1.sacarFoto();
-celular1.sacarFoto();
+celular1.grabarVideo();
 celular1.reiniciar();
+celular1.botonDeEncender();
