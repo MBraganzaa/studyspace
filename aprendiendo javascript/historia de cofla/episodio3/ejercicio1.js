69498c7edd60b8c4bd25e2744d8e508e7590b5c0
MBraganzaa <josebraganza23@gmail.com>
2022-08-05 00:59:51 -0300

    episodio 3 cofla (ejercicio poo)

--- aprendiendo javascript/historia de cofla/episodio3/ejercicio1.js
+++ aprendiendo javascript/historia de cofla/episodio3/ejercicio1.js
@@ -37,14 +37,46 @@ class Celular {
             alert("el celular esta apagado");
         }
     }
+    mostrarInfo (){
+        return `
+        color : <b>${this.color}</b><br>
+        peso : <b>${this.peso}</b><br>
+        resolucion de pantalla : <b>${this.resolucionDePantalla}</b><br>
+        resolucion de camara : <b>${this.resolucionDeCamara}</b><br>
+        memoria ram : <b>${this.ram}</b><br>
+        `;
+    } 
 }
 
-celular1 = new Celular ("rojo","150g","5.5'","hd","1GB");
-celular2 = new Celular ("gray","155g","5'","full hd","2GB");
-celular3 = new Celular ("black","140g","5.4'","full hd","4GB");
+class MovileAlta extends Celular {
+    constructor(color,peso,rdp,rdc,ram,rdce){
+    super(color,peso,rdp,rdc,ram);
+    this.resolucionDeCamaraExtra = rdce;
+    }
+    grabarVideoLento(){
+        alert("grabando video en modo lento");
+    }
+    reconocimientoFacial(){
+        alert("reconocimiento facial ACTIVADO acerquese a la pantalla");
+    }
+    infoAltaGama(){
+        return `${this.mostrarInfo()} resolucion de camara Extra : ${this.resolucionDeCamaraExtra} <br>`;
+    }
+}
 
-celular1.botonDeEncender()
-celular1.sacarFoto();
-celular1.grabarVideo();
-celular1.reiniciar();
-celular1.botonDeEncender();
+
+celular1 = new MovileAlta ("rojo","150g","5.5'","hd","1GB","4k");
+celular2 = new MovileAlta ("gray","155g","5'","full hd","2GB","hd");
+celular3 = new MovileAlta ("black","140g","5.4'","full hd","4GB","full hd");
+
+// celular1.botonDeEncender()
+// celular1.sacarFoto();
+// celular1.grabarVideo();
+// celular1.reiniciar();
+// celular1.botonDeEncender();
+
+document.write(`
+${celular1.infoAltaGama()}</br>
+${celular2.infoAltaGama()}</br>
+${celular3.infoAltaGama()}</br>
+`);
