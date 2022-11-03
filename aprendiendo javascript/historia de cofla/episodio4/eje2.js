5efc08d8f850a6fa1111956541df8a8f41fae222
MBraganzaa <josebraganza23@gmail.com>
2022-11-03 17:32:19 -0300

    commit(eje2 de dalto java junior. cap4)

--- aprendiendo javascript/historia de cofla/episodio4/eje2.js
+++ aprendiendo javascript/historia de cofla/episodio4/eje2.js
@@ -1,19 +1,46 @@
 const obtenerInformacion = (materia)=>{
     materias = {
-        fisica: ["manu","pedro","dalto","jose","martin"],
-        matematicas: ["manu","pedro","dalto","jose","martin"],
-        programacion: ["manu","pedro","dalto","jose","martin"],
-        gimnacio: ["manu","pedro","dalto","jose","martin"]
+        fisica: ["pablo","manu","pedro","martin"],
+        matematicas: ["mariana","manu","dalto","jose","martin"],
+        programacion: ["silvia","manu","pedro","dalto","jose","martin"],
+        gimnacio: ["paolo","pedro","jose","martin"]
     }
     if (materias[materia] !== undefined){
-        return [materias[materia],materia];
+        return [materias[materia],materia,materias];
     } else {
-        return false;
+        return materias;
     }
 }
 
-let informacion = obtenerInformacion("fisica");
+const mostrarInformacion = (materia)=>{
+    let informacion = obtenerInformacion(materia);
 
 if (informacion !== false){
-    document.write(`alumnos presentes en la materia ${informacion[1]} : ${informacion[0]}`);
+    let profesor = obtenerInformacion(materia)[0][0];
+    alumnos = obtenerInformacion(materia)[0];
+    alumnos.shift();
+    document.write(`la materia es: ${informacion[1]}, y su profesor es: ${profesor},<br>
+    y  los alumnos presentes son : ${alumnos}<br><br>`);
 }
+}
+
+mostrarInformacion("fisica");
+mostrarInformacion("matematicas");
+mostrarInformacion("programacion");
+mostrarInformacion("gimnacio");
+
+const cantidadDeClases = (alumno)=>{
+    let informacion = obtenerInformacion();
+    let cantidadTotal = 0;
+    let cursando = [];
+    for (info in informacion){
+        if(informacion[info].includes(alumno)){
+            cantidadTotal++;
+            cursando.push(" "+ info);
+        }
+    }
+    return `${alumno} esta en ${cantidadTotal} clases.<br>
+    y esta cursando: ${cursando}<br><br> `;
+}
+
+document.write(cantidadDeClases("manu"));
